f77d55ac08dc2ff3638342b5190973337401f7e4
Wilhams Gomes <71606861+WilhamsGomes@users.noreply.github.com>
2023-04-07 22:44:40 -0300

    Novos recursos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,53 +1,75 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
 	Container,
 	InputSearchContainer,
 	Header,
-	ListContainer,
+	ListHeader,
 	Card,
 } from './style';
 
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
-// import Modal from '../../components/Modal';
-// import Loader from '../../components/Loader';
 
 export default function Home() {
+	const [contacts, setContacts] = useState([]);
+	const [orderBy, setOrderBy] = useState('asc');
+
+	useEffect(() => {
+		fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
+			.then(async (response) => {
+				const json = await response.json();
+				setContacts(json);
+			})
+			.catch((error) => {
+				console.log('error', error);
+			});
+	}, [orderBy]);
+
+	function handleToggleOrderBy() {
+		setOrderBy(
+			(prevState) => (prevState === 'asc' ? 'desc' : 'asc'),
+		);
+	}
+
 	return (
 		<Container>
-			{/* <Modal danger /> */}
-			{/* <Loader /> */}
-
 			<InputSearchContainer>
 				<input type="text" placeholder="Pesquise pelo nome..." />
 			</InputSearchContainer>
 			<Header>
-				<strong>3 contatos</strong>
+				<strong>
+					{contacts.length}
+					{contacts.length === 1 ? ' contatos' : ' contato'}
+				</strong>
 				<Link to="/new">
 					Novo contato
 				</Link>
 			</Header>
-			<ListContainer>
-				<header>
-					<button type="button">
+
+			<ListHeader orderBy={orderBy}>
+					<button type="button" onClick={handleToggleOrderBy}>
 						<span>Nome</span>
 						<img src={arrow} alt="Arrow" />
 					</button>
-				</header>
+			</ListHeader>
 
-				<Card>
+			{contacts.map((contact) => (
+				<Card key={contact.id}>
 					<div className="info">
 						<div className="contact-name">
-							<strong>Mateus Silva</strong>
-							<small>instagram</small>
+							<strong>{contact.name}</strong>
+							{contact.category_name && (
+								<small>{contact.category_name}</small>
+							)}
 						</div>
-						<span>matues@devacademy.com.br</span>
-						<span>(41) 09999-9999</span>
+						<span>{contact.email}</span>
+						<span>{contact.phone}</span>
 					</div>
 
 					<div className="actions">
-						<Link to="/edit/123">
+						<Link to={`/edit/${contact.id}`}>
 							<img src={edit} alt="Edit" />
 						</Link>
 						<button type="button">
@@ -55,16 +77,18 @@ export default function Home() {
 						</button>
 					</div>
 				</Card>
-
-			</ListContainer>
+			))}
 		</Container>
 	);
 }
 
-// fetch('http://localhost:3000')
-// 	.then((response) => {
-// 		console.log('response', response);
-// 	})
-// 	.catch((error) => {
-// 		console.log('error', error);
-// 	});
+// SOP -> Same Origin Policy -> Política de mesma origem
+// CORS -> Cross-Origin Resource Sharing -> Compartilhamento de recursos entre origens diferentes
+// Origem: protocolo://domínio:porta
+
+//  Saída : http://http://localhost:3000
+// Destino: http://http://localhost:3001
+
+// Preflight -> Pré-voô
+// OPTIONS http://localhost:3001/contacts
+// -> Verifica se a origem e os headers que estamos tentando enviar são permitidos
